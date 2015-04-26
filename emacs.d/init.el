f1db6ca57454ea137357a3413c1ae14dcc64f25d
Emil Eriksson <emil.eriksson@codemill.se>
2015-04-26 16:11:17 +0200

    Ninja commit of some stuff

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,4 @@
-;;; INIT.el --- Cusomizations for Emacs
+;;; Init.el --- Cusomizations for Emacs
 ;;; Commentary:
 ;;; Code:
 
@@ -10,7 +10,8 @@
       (tool-bar-mode -1) ; Hiding toolbar
       (scroll-bar-mode -1) ; Hide scrollbars
       ;(tabbar-mode -1) ; Disable tabbar
-      ))
+	  ))
+;(menu-bar-mode -1)
 
 ;(aquamacs-autoface-mode -1)
 ;(set-face-attribute 'mode-line nil :inherit 'unspecified)
@@ -19,44 +20,57 @@
 (setq inhibit-startup-message t
 	  initial-scratch-message nil)
 
-
-;;;; CUSTOMIZE
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ediff-split-window-function (quote split-window-horizontally))
- '(show-paren-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:height 110 :width normal :foundry "nil" :family "Menlo"))))
- '(mode-line ((t (:box nil))))
- '(mode-line-buffer-id ((t (:foreground "White"))))
- '(mode-line-emphasis ((t (:inverse-video t :weight bold)))))
+; Include column number int mode-line
+(setq column-number-mode t)
+(setq line-number-mode t)
 
 ;;;; ENVIRONMENT
 (add-to-list 'load-path "~/.emacs.d/vendor/")
+(add-to-list 'load-path "~/.emacs.d/estimation/")
 (add-to-list 'load-path "~/.emacs.d/vendor/evil-rebellion/")
+(add-to-list 'custom-theme-load-path (file-name-as-directory "~/.emacs.d/replace-colorthemes/"))
 (let
     ((path '("/usr/local/bin" "/usr/bin" "/bin"
-	     "/usr/local/sbin" "/usr/sbin" "/sbin"
-	     "/opt/X11/bin" "/opt/ImageMagick/bin"
-	     "/usr/local/MacGPG2/bin" "/usr/texbin")))
+			 "/usr/local/sbin" "/usr/sbin" "/sbin"
+			 "/opt/X11/bin" "/opt/ImageMagick/bin"
+			 "/usr/local/MacGPG2/bin" "/usr/texbin")))
   (progn
     (set 'exec-path (delete-dups (append path exec-path)))
     (setenv "PATH" (mapconcat 'identity path ":"))))
 
 ;;;; PACKAGES
-(defmacro after (mode &rest body)
-  `(eval-after-load ,mode
-     '(progn ,@body)))
+
+;;; Add package repository
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+; Make sure that we have use-package installed
+(if (not (package-installed-p 'use-package))
+	(progn
+	  (package-refresh-contents)
+	  (package-install 'use-package)))
+(require 'use-package)
+
+(require 'uniquify)
+(setq-default mode-line-buffer-identification
+			  `(:eval
+				(let ((s (format-mode-line
+						  (propertized-buffer-identification (buffer-name)))))
+				  (when (and (boundp 'uniquify-managed) uniquify-managed)
+					(unless (string= (buffer-name) (uniquify-buffer-base-name))
+					  (let ((base-len (length (uniquify-buffer-base-name)))
+							(full-len (length (buffer-name)))
+							(pre (eq uniquify-buffer-name-style 'forward)))
+						(let ((start (if pre 0 base-len))
+							  (end (if pre (- full-len base-len) full-len)))
+						  (set-text-properties start end nil s)))))
+				  s)))
 
 ; Pymacs in ~/.emacs.d/vendor/ (for ropemacs)
+
+;;;;; Auto complete
 (with-eval-after-load "auto-complete"
   (add-to-list 'ac-dictionary-directories "~/.emacs.d/dict")
   (setq ac-use-menu-map t)
@@ -85,14 +99,71 @@
   (message "After auto-complete-autoloads")
   )
 
+;;;;; Diminish
+
+(require 'diminish)
+;Perhaps replace with rich-minority https://github.com/Bruce-Connor/rich-minority
+
+(with-eval-after-load "afafafdiminish"
+  (with-eval-after-load "adaptive-wrap-prefix"
+	(diminish 'adaptive-wrap-prefix-mode))
+  (diminish 'hs-minor-mode)
+  (with-eval-after-load "projectile"
+	(diminish 'projectile-mode "P"))
+  (diminish 'undo-tree-mode)
+  (with-eval-after-load "ws-trim"
+	(diminish 'ws-trim-mode)))
+
+;;;;; Estimation
+
+(require 'estimation)
+
+(with-eval-after-load "estimation"
+  (add-hook 'estimation-mode-hook
+			(lambda () (progn
+						 (hs-minor-mode 1)
+						 (visual-line-mode 1)
+						 (adaptive-wrap-prefix-mode 1)))))
+
+;;;;; Evil
+
 (defun my-move-key (keymap-from keymap-to key)
-  "Moves key binding from one keymap to another, deleting from the old location. "
+  "Move key binding from one keymap to another, deleting from the old location."
   (define-key keymap-to key (lookup-key keymap-from key))
   (define-key keymap-from key nil))
 
+(require 'evil)
+
+;;; esc quits
+;(define-key evil-normal-state-map [escape] 'keyboard-quit)
+;(define-key evil-visual-state-map [escape] 'keyboard-quit)
+;(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+;(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+;(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+;(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+;(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+
+(defun ex-mode-mapping (cmd)
+  (let ((binding (car cmd))
+		(fn (cdr cmd)))
+	(evil-ex-define-cmd binding fn)))
+
 (with-eval-after-load "evil"
+  (evil-mode t)
+
   (define-key evil-normal-state-map "," nil)
   (define-key evil-normal-state-map "\C-j" 'evil-jump-to-tag)
+  (define-key evil-normal-state-map "\C-k" 'evil-jump-backward)
+
+  (my-move-key evil-motion-state-map evil-normal-state-map (kbd "RET"))
+  (my-move-key evil-motion-state-map evil-normal-state-map " ")
+
+  (mapc 'ex-mode-mapping
+		'(("gstatus" . magit-status)
+		  ("whitespace" . whitespace-mode)
+		  ("test" . projectile-test-project)
+		  ("ack" . ack-and-a-half)))
+
   (require 'evil-rebellion)
   (require 'evil-leader)
   (require 'evil-surround))
@@ -104,26 +175,9 @@
 (with-eval-after-load "evil-surround"
   (global-evil-surround-mode 1))
 
-(defun imenu-elisp-sections ()
-  (setq imenu-prev-index-position-function nil)
-  (add-to-list 'imenu-generic-expression '("Section" "^;;;; \\(.+\\)$" 1) t))
-(add-hook 'emacs-lisp-mode-hook 'imenu-elisp-sections)
-(with-eval-after-load "helm-config"
-  (with-eval-after-load "evil"
-    (define-key evil-normal-state-map ", f" 'helm-find-files)
-    (define-key evil-normal-state-map ", n" 'helm-projectile)
-    (define-key evil-normal-state-map  ", o" 'helm-imenu)
-    (define-key evil-normal-state-map ", b" 'helm-buffers-list)))
-
-(with-eval-after-load "projectile"
-  (projectile-global-mode)
-  (define-key evil-normal-state-map ",ps" 'projectile-switch-project))
-
-(with-eval-after-load "magit"
-  (define-key evil-normal-state-map ",gs" 'magit-status))
-
-; (with-eval-after-load "neotree"
-;   (global-set-key [f2] 'neotree-toggle))
+;;;;; Flycheck
+;;; Enable syntax-checking with flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
 
 ; (with-eval-after-load "flycheck"
 ;   (flycheck-define-checker python3-pyflakes
@@ -137,16 +191,102 @@
 ; 
 ;   (add-to-list 'flycheck-checkers 'python3-pyflakes))
 
-;;; Add package repository
-(require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
-
+;;;;; Flyspell
 (with-eval-after-load "flyspell"
   (setq flyspell-issue-welcome-flag nil
 		ispell-program-name "aspell"
 		ispell-list-command "list"))
+
+;;;;; Haskell
+
+;; Sane indentation for haskell
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
+;;;;; Helm
+
+(defun imenu-elisp-sections ()
+  ;(setq imenu-prev-index-position-function nil)
+  (add-to-list 'imenu-generic-expression '("Section" "^;;;; \\(.+\\)$" 1) t)
+  (add-to-list 'imenu-generic-expression '("Subsection" "^;;;;; \\(.+\\)$" 1) t))
+(add-hook 'emacs-lisp-mode-hook 'imenu-elisp-sections)
+
+(with-eval-after-load "helm-config"
+
+  (require 'helm-projectile)
+
+  (with-eval-after-load "evil"
+
+    (define-key evil-normal-state-map ", f" 'helm-find-files)
+    (define-key evil-normal-state-map  ", o" 'helm-imenu)
+    (define-key evil-normal-state-map ", b" 'helm-buffers-list)
+
+	(with-eval-after-load "projectile"
+	  (define-key evil-normal-state-map ", n" 'helm-projectile)
+	  (define-key evil-normal-state-map  ", p" 'helm-projectile-switch-project))
+	
+	; Fix for updated projectile
+	(defalias 'helm-buffers-list--match-fn 'helm-buffer-match-major-mode)))
+
+(require 'helm-config)
+
+;;;;; Ido
+
+(ido-mode 1)
+(setq ido-everywhere 1)
+
+(with-eval-after-load "ido"
+  (setq ido-use-faces nil) ; Use flx hightlits instead of ido faces
+
+  ;; Fuzzy matching for ido with flx-ido
+  (require 'flx-ido)
+
+  (require 'ido-ubiquitous) ; Ido everywhere!!!
+
+  ;; Vertical list for ido
+  (require 'ido-vertical-mode))
+
+(with-eval-after-load "flx-ido" (flx-ido-mode 1))
+
+(with-eval-after-load "ido-vertical-mode" (ido-vertical-mode 1))
+
+(with-eval-after-load "ido-ubiquitous" (ido-ubiquitous-mode 1))
+
+;;;;; Magit
+
+(require 'magit)
+
+(with-eval-after-load "magit"
+  (setq magit-commit-show-notes t)
+  (setq magit-last-seen-setup-instructions "1.4.0")
+  (with-eval-after-load "evil"
+	(define-key evil-normal-state-map ",gs" 'magit-status)))
+
+;;;;; Modeline-posn
+
+(require 'modeline-posn)
+(setq modelinepos-column-limit 70)
+
+;;;;; Neotree
+
+; (with-eval-after-load "neotree"
+;   (global-set-key [f2] 'neotree-toggle))
+
+;;;;; Projectile
+
+(require 'projectile)
+
+(with-eval-after-load "projectile"
+  (projectile-global-mode t)
+  (with-eval-after-load "evil"
+	(define-key evil-normal-state-map ",ps" 'projectile-switch-project)))
+
+;;;;; Sr Speedbar
+
+(require 'sr-speedbar)
+
+(with-eval-after-load "sr-speedbar"
+  (global-set-key [f2] 'sr-speedbar-toggle))
+
 
 ;;;; BEHAVIOR
 ;; Fix option-key
@@ -156,29 +296,80 @@
       mac-allow-antialiasing t
       mac-command-key-is-meta t)
 
-(global-set-key [f2] 'speedbar)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
 
-(require 'helm-config)
+(setq tags-revert-without-query t)
+
+;(global-set-key [f2] 'speedbar)
 
 (setq-default tab-width 4)
 (smart-tabs-insinuate 'c
 					  'c++)
 
+
+;;; Enable yasnippets
+;(add-hook 'prog-mode-hook #'yas-minor-mode)
+
+(server-start)
+
+;;;; NON-PACKAGES
+
+(require 'ws-trim)
+
+(with-eval-after-load "ws-trim"
+  (global-ws-trim-mode 1)
+  (setq ws-trim-mode 1))
+
+;;;; EXTENSIONS
+
+; Spelling stuff
+(with-eval-after-load "evil"
+  (define-key evil-normal-state-map ",ss" 'flyspell-mode)
+  (define-key evil-normal-state-map ",sl" 'ispell-change-dictionary))
+
+
+(defun enable-tabs ()
+  "Enables indentation with tabs."
+  (interactive)
+  (setq indent-tabs-mode t))
+
+
+(defvar marked-name)
 (setq marked-name "Marked 2")
 (defun markdown-preview-file ()
-  "run Marked on the current file and revert the buffer"
+  "Run Marked on the current file and revert the buffer."
   (interactive)
   (shell-command (format "open -a '%s' '%s'"
 						 marked-name
-						 (buffer-file-name)))
-)
+						 (buffer-file-name))))
 
-;;; VIM FOREVER
-(require 'evil)
-(evil-mode t)
-(my-move-key evil-motion-state-map evil-normal-state-map (kbd "RET"))
-(my-move-key evil-motion-state-map evil-normal-state-map " ")
 
+(defun copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
+
+(with-eval-after-load "projectile"
+  (defun copy-project-file-name-to-clipboard ()
+	"Copy the current buffer path to the clipboard"
+	(interactive)
+	(let ((filename (buffer-file-name))
+		  (root-length (if (projectile-project-p)
+						   (length (projectile-project-root))
+						 0)))
+	  (let ((project-filename (substring filename root-length)))
+		(kill-new project-filename)
+		(message "Copied buffer file name '%s' to the clipboard." project-filename)))))
+  
+
+;;;; THEME
+(load-theme 'flatui t)
+(global-hl-line-mode)
 
 ; Switch mode-line color from flatuicolors.com
 (setq green-sea "#16a085" turquoise "#1abc9c" turquoise-hover "#1ABC9C"
@@ -190,11 +381,11 @@
       pumpkin "#d35400" carrot "#e67e22" carrot-hover "#e98b39"
       pomegranate "#c0392b" alizarin "#e74c3c" alizarin-hover "#ea6153"
       silver "#bdc3c7" clouds "#ecf0f1" cluds-hover "#fbfcfc"
-      asbestos "#7f8c8d" concrete "#95a5a6" concrete-hover "#a3b1b2"
-      )
+      asbestos "#7f8c8d" concrete "#95a5a6" concrete-hover "#a3b1b2")
 
 (set-face-background 'mode-line-inactive silver)
 (set-face-foreground 'mode-line-inactive asbestos)
+(set-face-attribute 'mode-line-inactive nil :box nil)
 
 (let ((current-color (lambda ()
 		       (let ((color (cond ((minibufferp) `(,wisteria . ,amethyst-hover))
@@ -211,32 +402,30 @@
 			 ))))
   (add-hook 'post-command-hook current-color))
 
-;(require 'smart-mode-line)
-;(sml/setup)
-;(sml/apply-theme 'dark)
-;(add-to-list 'sml/replacer-regexp-list '(":Doc:Codemill/Burberry/" ":BRB:") t)
+; Previous font '(default ((t (:height 110 :width normal :foundry "nil" :family "Menlo"))))
+;;;; CUSTOMIZE
 
-;;; Enable syntax-checking with flycheck
-(add-hook 'after-init-hook #'global-flycheck-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ecb-options-version "2.40")
+ '(ediff-split-window-function (quote split-window-horizontally))
+ '(show-paren-mode t)
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify))
+ '(uniquify-separator ":"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 90 :width normal :foundry "nil" :family "ProfontIIx"))))
+ '(mode-line ((t (:box nil))))
+ '(mode-line-buffer-id ((t (:foreground "White"))))
+ '(mode-line-emphasis ((t (:inverse-video t :weight bold))))
+ '(modelinepos-column-warning ((t (:foreground "white"))))
+ '(modelinepos-region ((t (:foreground "white")))))
 
-;; Sane indentation for haskell
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-
-;;; Enable yasnippets
-;(add-hook 'prog-mode-hook #'yas-minor-mode)
-
-;;; Ido hacking
-(setq ido-use-faces nil) ; Use flx hightlits instead of ido faces
-
-;; Fuzzy matching for ido with flx-ido
-(require 'flx-ido)
-(flx-ido-mode 1)
-;; Vertical list for ido
-(require 'ido-vertical-mode)
-(ido-vertical-mode 1)
-
-(require 'ido-ubiquitous) ; Ido everywhere!!!
-(ido-ubiquitous-mode 1)
-
-(ido-mode 1)
-(ido-everywhere 1)
+(provide 'init)
+;;; init.el ends here
