13002d936dfe9bbc7d492702f09d5f6b08bc0306
Emil Eriksson <asheidan@gmail.com>
2019-02-26 22:37:50 +0100

    Byte-compile tangled config.org

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -27,15 +27,53 @@
       (string-equal system-type "darwin")))
     (menu-bar-mode -1))
 
-(require 'package)
+(package-initialize)
+;(require 'package)
 ;;; Add package repository
 (add-to-list 'package-archives
-			 '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
+(eval-when-compile
+;  (add-to-list 'load-path "~/.emacs.d/elpa")
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
 
-(org-babel-load-file (concat user-emacs-directory "config.org"))
+(require 'ob-tangle)
+(defun my/org-babel-load-file (file &optional compile)
+  "Load Emacs Lisp source code blocks in the Org FILE.
+This function exports the source code using `org-babel-tangle'
+and then loads the resulting file using `load-file'.  With prefix
+arg (noninteractively: 2nd arg) COMPILE the tangled Emacs Lisp
+file to 'byte-code' before it is loaded."
+  (interactive "fFile to load: \nP")
+  (let* ((age (lambda (file)
+                (float-time
+                 (time-subtract (current-time)
+                                (nth 5 (or (file-attributes (file-truename file))
+                                           (file-attributes file)))))))
+         (base-name (file-name-sans-extension file))
+         (exported-file (concat base-name ".el"))
+         (compiled-file (if compile
+                            (concat exported-file "c")
+                          exported-file)))
+    ;; tangle if the Org file is newer than the elisp file
+    (unless (and (file-exists-p exported-file)
+                 (> (funcall age file) (funcall age exported-file)))
+      ;; Tangle-file traversal returns reversed list of tangled files
+      ;; and we want to evaluate the first target.
+      (setq exported-file
+            (car (last (org-babel-tangle-file file exported-file "emacs-lisp")))))
+    ;; byte-compile if we should compile and compiled file is older
+    (if (and compile
+             (not (and (file-exists-p compiled-file)
+                       (> (funcall age exported-file) (funcall age compiled-file)))))
+	     (progn (byte-compile-file exported-file 'load)
+		    (message "Compiled and loaded %s" compiled-file))
+	(progn (load-file compiled-file)
+               (message "Loaded %s" compiled-file)))))
 
+(my/org-babel-load-file (concat user-emacs-directory "config.org") 'compile)
 
 (put 'narrow-to-region 'disabled nil)
 
